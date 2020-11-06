0f9afe974304979af82c9db7ee1c6ae06fa63058
Andrei Sas <andreisas007@gmail.com>
2020-11-06 14:37:16 +0100

    Initial commit

---
+++ src/emailapp/Email.java
@@ -0,0 +1,33 @@
+package emailapp;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String pasword;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    
+    public Email(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+
+        //Call a method asking for the department - return department
+    }
+
+
+    //Ask for department
+
+    //Generate random password
+
+    //Set the mailboxCapacity
+
+    //Set alternateEmail
+
+    //Change the password
+
+
+
+}
